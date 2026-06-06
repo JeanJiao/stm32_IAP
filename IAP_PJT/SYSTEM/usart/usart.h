c21dd7f1765389f63ce40cd3f5f961d651241332
jean <jean_jiao@163.com>
2026-06-06 17:10:37 +0800

    添加LCD模块, 后续用于调试

--- IAP_PJT/SYSTEM/usart/usart.h
+++ IAP_PJT/SYSTEM/usart/usart.h
@@ -2,6 +2,9 @@
 #define __USART_H
 #include "stdio.h"	
 #include "sys.h" 
+#include "config.h"
+#include "common.h"
+#include "led.h"
 //////////////////////////////////////////////////////////////////////////////////	 
 //本程序只供学习使用，未经作者许可，不得用于其它任何用途
 //ALIENTEK STM32开发板
